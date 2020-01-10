ed5c321d79f1c5d8a593a0208a758025225a32c6
Shubham Kulkarni <58355613+shubhamsk894@users.noreply.github.com>
2020-01-10 11:42:18 +0530

    Update storage2.js

--- src/utils/storage2.js
+++ src/utils/storage2.js
@@ -1,23 +1,24 @@
 import compose from "lodash/fp/compose";
 import curry from "lodash/fp/curry";
+import map from "lodash/fp/map";
 
-const _bind = (fn, context) => fn.bind(context);
-const resolve = _bind(Promise.resolve, Promise);
+const _bind = (fn, cxt) => fn.bind(cxt);
 const { parse, stringify } = JSON;
 const getItem = _bind(localStorage.getItem, localStorage);
 const setItem = curry(_bind(localStorage.setItem, localStorage));
+const _delete = key => delete localStorage[key];
 
 const storage = {
-  get: async key => {
-    let result = await getItem(key);
-    return result;
-  },
-  set: async (key, value) => {
-    let result = await setItem(key, stringify(value));
-    return result;
-  }
+  get: async key =>
+    await compose(
+      parse,
+      getItem
+    )(key),
+  set: async (key, value) =>
+    await compose(
+      setItem(key),
+      stringify
+    )(value),
+  delete: async key => await compose(_delete)(key)
+  //setItem(key, stringify(value))
 };
-localStorage["1"] = "a";
-storage.get("1").then(console.log);
-storage.set("2", "b");
-storage.get("2").then(console.log);
